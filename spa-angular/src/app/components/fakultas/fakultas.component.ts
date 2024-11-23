0d86f97d1482ce105c554cfc239b591a0c833b82
nabilasusan <nabila_2226240101@mhs.mdp.ac.id>
2024-11-23 09:18:00 +0700

    spa angular

---
+++ spa-angular/src/app/components/fakultas/fakultas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fakultas',
+  imports: [],
+  templateUrl: './fakultas.component.html',
+  styleUrl: './fakultas.component.css'
+})
+export class FakultasComponent {
+
+}
---
+++ spa-angular/src/app/components/prodi/prodi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prodi',
+  imports: [],
+  templateUrl: './prodi.component.html',
+  styleUrl: './prodi.component.css'
+})
+export class ProdiComponent {
+
+}
